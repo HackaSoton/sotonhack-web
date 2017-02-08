1c9540183451e8036d5e4c81bdbfbf8df9875282
Jamie Scott <github@jami.org.uk>
2017-02-08 22:20:21 +0000

    Changed header theme from dark to light

--- js/creative.js
+++ js/creative.js
@@ -68,7 +68,7 @@
 
 // Load header image
 $(document).ready(function() {
-    $("header").css("background", "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('img/header.jpg')");
+    $("header").css("background", "linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('img/header.jpg')");
     $("header").css("background-size", "cover");
 });
 
